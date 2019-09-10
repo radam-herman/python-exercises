cbaaac28bb2b76ef32f16ae7a8f9121199acb5b9
Adam H <raymond.a.herman@gmail.com>
2019-09-10 16:04:52 -0500

    past TYPE Checks

---
+++ 1st_exercises_in_class.py
@@ -0,0 +1,28 @@
+type(99.9) # class float
+type("False")
+type(False)
+type('0')
+type(0)
+type(True)
+type('True')
+type([{}])
+type({'a': []})
+# """>>> type(99.9) # class float
+<class 'float'>
+>>> type("False")
+<class 'str'>
+>>> type(False)
+<class 'bool'>
+>>> type('0')
+<class 'str'>
+>>> type(0)
+<class 'int'>
+>>> type(True)
+<class 'bool'>
+>>> type('True')
+<class 'str'>
+>>> type([{}])
+<class 'list'>
+>>> type({'a': []})
+<class 'dict'>"""
+
