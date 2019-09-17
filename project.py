b9a6dfcc60e3febb9a42a7f2e18a0c736190579a
Adam H <raymond.a.herman@gmail.com>
2019-09-17 09:12:40 -0500

    have menu - working on definitions

--- project.py
+++ project.py
@@ -29,15 +29,33 @@
         # remove
     # also look at clearing screen before reprint of menu.
 
+def check_balance():
+    print("this is the balance checker place holder")
+
+def withdrawal():
+    print("this is the withrawal place holder")
+
+def deposit():
+    print("this is the deposit")
+
+def exit():
+    print("you chose to exit - are you sure?")
+    check_choice = input("Enter yes or no: ")
+    #if check_choice.lower() == "yes":
+    #   return loop = False
+    #elif check_choice.lower() == "no":
+    #    return loop = True
+    #else:
+    # #   return raw_input("Wrong option selection. Enter any key to try again..")
+
 
 
 def print_menu():       ## Your menu design here
     print(30 * "-" , "MENU" , 30 * "-")
-    print("1. Menu Option 1")
-    print("2. Menu Option 2")
-    print("3. Menu Option 3")
-    print("4. Menu Option 4")
-    print("5. Exit")
+    print("1. view current balance")
+    print("2. add a debit (withdrawal)")
+    print("3. add a credit (deposit)")
+    print("4. Exit")
     print(67 * "-")
   
 loop=True      
@@ -47,21 +65,28 @@ while loop:          ## While loop which will keep going until loop = False
     choice = input("Enter your choice [1-5]: ")
      
     if int(choice) == 1:     
-        print("Menu 1 has been selected")
+        print("Menu 1 - current balance has been selected")
         ## You can add your code or functions here
+        check_balance()
     elif int(choice) == 2:
-        print("Menu 2 has been selected")
+        print("Menu 2 - add a debit (withdrawal) has been selected")
         ## You can add your code or functions here
+        withdrawal()
     elif int(choice) == 3:
-        print("Menu 3 has been selected")
+        print("Menu 3 - add a credit (deposit) has been selected")
         ## You can add your code or functions here
+        deposit()
     elif int(choice) == 4:
         print("Menu 4 has been selected")
         ## You can add your code or functions here
-    elif int(choice) == 5:
-        print("Menu 5 has been selected")
-        ## You can add your code or functions here
-        loop=False # This will make the while loop to end as not value of loop is set to False
+        loop=False # This will make the while loop to end as 
+        # not value of loop is set to False
+        exit()
     else:
         # Any integer inputs other than values 1-5 we print an error message
         raw_input("Wrong option selection. Enter any key to try again..")
+
+
+
+
+
